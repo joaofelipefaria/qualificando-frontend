653e7ac106551347457d55ec100384fb5d722a07
joaofelipefaria <jfelipe.web@gmail.com>
2026-09-14 17:29:59 -0300

    correcao da url do backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMockApi: false,
-  apiUrl: 'https://qualificando-backend-ffkwp6j5ia-rj.a.run.app/api/v1',
-  version: '0.1.00b-prod'
+  apiUrl: 'https://qualificando-backend-3dw5himw6q-rj.a.run.app/api/v1',
+  version: '0.1.01-prod'
 };
